8a617aeb3a19d7699dd8df678a6f17ff70b668ea
anu2arc <asingh4456@gmail.com>
2022-04-18 11:56:07 +0530

    template file created

---
+++ Template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define mod 1e9 + 7
+
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        
+    }
+    return 0;
+}
